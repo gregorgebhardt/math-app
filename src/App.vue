40d1f305f135cb9cc005270a715ececab7d5d54f
Gregor Gebhardt <gregor.gebhardt@mercedes-benz.com>
2026-05-29 17:45:08 +0200

    feat: wire up App shell with picker and quiz routing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="app">
-    <h1>Mathe Übungen</h1>
-    <p>Wird geladen...</p>
+    <QuizPicker v-if="!activeQuiz" @start="onStart" />
+    <component
+      v-else
+      :is="activeQuiz.component"
+      v-bind="activeQuiz.options"
+      @back="activeQuiz = null"
+    />
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { quizzes } from './quizzes.js'
+import QuizPicker from './components/QuizPicker.vue'
+
+const activeQuiz = ref(null)
+
+function onStart({ quizKey, options }) {
+  const quiz = quizzes[quizKey]
+  if (!quiz) return
+  activeQuiz.value = { component: quiz.component, options }
+}
 </script>
